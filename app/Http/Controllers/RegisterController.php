cf0da366311df6c7f569e49a8c62d5d2a7985f18
Marcos-mi <marcosminof22@gmail.com>
2022-10-21 12:28:39 -0300

    finalizando el registro de usuarios

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -14,14 +17,25 @@ class RegisterController extends Controller
     //    dd($request);
     // dd($request->get('username'));
 
+    //modificar el request
+        $request-> request ->add(['username' => Str::slug( $request-> username)]);
     // Validación
         $this-> validate($request,[
             'name' => "required|max:30",
-            'username' => "required|unique:users|min:3|max:16",
+            'username' =>  "required|unique:users|min:3|max:16",
             'email' => "required|unique:users|min:3|max:45|email",
             'password' => "required|confirmed|min:6",
         ]);
 
-        dd('creando usuario');
+        User::create([
+            'name' => $request-> name,
+            'username' => $request-> username,
+            'email' => $request-> email,
+            'password' => Hash::make( $request-> password)
+        ]);
+
+        //redireccionar
+
+        return redirect()-> route('posts.index');
     }
 }
